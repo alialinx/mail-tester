7f17223ffe42b92758b845acfb47a3b8f9fcd806
alialinxz <alialinxz@gmail.com>
2026-01-01 22:22:08 +0300

    feat(users): created login and logout api

--- src/api/users.py
+++ src/api/users.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timezone
 
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 
-from src.api.functions import get_request_info, hash_password, system_log
+from src.api.functions import get_request_info, hash_password, system_log, verify_password
 from src.api.schema import UserRegister
+from src.api.token import get_active_or_new_token
 from src.db.db import get_db
 
 router = APIRouter()
@@ -38,3 +40,36 @@ def register(info:UserRegister, db=Depends(get_db), req_info=Depends(get_request
 
     return {"success": True, "user_id": str(user_id), "message": "User registered"}
 
+
+
+@router.post("/login", summary="Login a user")
+def login(form_data: OAuth2PasswordRequestForm = Depends(),db=Depends(get_db), req_info=Depends(get_request_info)):
+
+    now = datetime.now(timezone.utc)
+
+    username = form_data.username
+    password = form_data.password
+
+    user = db.users.find_one({"email": username})
+
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    hashed_password = user.get("password_hash")
+
+    if not verify_password(password, hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    token, expires_at = get_active_or_new_token(user,db)
+
+    system_log(db,"login",user_id=user["_id"],request_info=req_info )
+
+    db.users.update_one({"email": username}, {"$set": {"last_login_at": now}, "token_expires_at":expires_at.isoformat()})
+
+    return {"success":True, "message":"login successfull", "access_token":token, "token_type":"bearer", "expires_at":expires_at.isoformat()}
+
+
+@router.get("/logout", summary="Logout a user")
+def logout():
+    return {"success": True, "message": "Logout successful"}
